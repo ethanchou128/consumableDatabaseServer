b18c8c28e8ff8833854e6d8ca759d83250125281
Ethan <elc10@sfu.ca>
2021-11-27 16:50:36 -0800

    initial commit

---
+++ webappserver/src/main/java/ca/cmpt213/a4/webappserver/WebappserverApplication.java
@@ -0,0 +1,13 @@
+package ca.cmpt213.a4.webappserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappserverApplication.class, args);
+	}
+
+}
